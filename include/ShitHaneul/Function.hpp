d13a08c96ca487e9862b9fabaef2d27c1010fc64
static <kmc7468@naver.com>
2020-08-30 01:55:48 +0900

    Josa 구조체, JosaList 클래스 추가
    Josa 구조체는 함수에서 필요한 조사와 조사의 해시를 저장하고, JosaList 클래스는 조사들을 저장합니다.

--- include/ShitHaneul/Function.hpp
+++ include/ShitHaneul/Function.hpp
@@ -2,8 +2,37 @@
 
 #include <ShitHaneul/ConstantList.hpp>
 
+#include <cstddef>
 #include <cstdint>
 #include <string>
+#include <utility>
+#include <vector>
+
+namespace ShitHaneul {
+	struct Josa final {
+		std::string String;
+		std::size_t Hash;
+	};
+
+	class JosaList final {
+	private:
+		std::vector<Josa> m_List;
+
+	public:
+		JosaList() noexcept = default;
+		JosaList(JosaList&& josaList) noexcept;
+		~JosaList() = default;
+
+	public:
+		JosaList& operator=(JosaList&& josaList) noexcept;
+		const Josa& operator[](std::uint8_t index) const noexcept;
+
+	public:
+		void AddJosa(std::string&& josa);
+		std::uint8_t GetCount() const noexcept;
+		void ReserveJosas(std::uint8_t count);
+	};
+}
 
 namespace ShitHaneul {
 	class FunctionInfo final {
